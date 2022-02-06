1107f2ce8ae8dd3cca2ecbfcad403f94ae309d9d
shire-kan <aylv.hgyr50@gmail.com>
2022-02-06 18:37:38 +0900

    マイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_06_082250) do
+ActiveRecord::Schema.define(version: 2022_02_06_093524) do
+
+  create_table "fixedcost_values", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "fixedcost_id", null: false
+    t.date "year_month"
+    t.integer "value"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "fixedcosts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
