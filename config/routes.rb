0be46415c316fb0e7f0f01b272b2fdf58a097c97
shire-kan <aylv.hgyr50@gmail.com>
2022-02-06 18:47:32 +0900

    固定費金額入力ルート修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'top#index'
   post "income_values/new(/:name)" => "income_values#new"
+  post "fixedcost_values/new(/:name)" => "fixedcost_values#new"
 
   resources :incomes
   resources :fixedcosts
